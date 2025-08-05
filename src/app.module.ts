c7d5c806da4044801acebdf5f3ddabd5cf218a01
dian <dianpayno2023@gmail.com>
2025-08-06 05:39:58 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Module } from '@nestjs/common';
@@ -6,12 +7,29 @@ import { ConfigModule } from '@nestjs/config';
 import { FeatureModules } from './features.module';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
-import { DBConfig } from './common/config';
+// import { DBConfig } from './common/config';
 import { CommonModule } from './common/common.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(DBConfig.config()),
+    // TypeOrmModule.forRoot(DBConfig.config()),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.DB_SYNC === 'TRUE',
+      logging: process.env.DB_LOGGING === 'TRUE',
+      autoLoadEntities: true,
+      ssl: true, // ✅ Tambahkan ini
+      extra: {
+        ssl: {
+          rejectUnauthorized: false, // ✅ Penting untuk Neon atau Railway
+        },
+      },
+    }),
     CommonModule,
     FeatureModules,
     WinstonModule.forRoot({
